ab994d54867ffbec0f7b1d8d03d5c184fca60e8e
Victor Jimenez <vjimenez@americatv.com.pe>
2016-05-21 09:12:59 -0500

    splahscreen

---
+++ app/src/main/java/pe/edu/upc/clinicaupc/SplashScreen.java
@@ -0,0 +1,52 @@
+package pe.edu.upc.clinicaupc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+    public static final int segundos = 200;
+    public static final int milisegundos = segundos * 8;
+    public static final int delay = 2;
+    private ProgressBar progreso;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        progreso = (ProgressBar) findViewById(R.id.progressBar);
+        progreso.setMax(maximo_progreso());
+        empezaranimacion();
+    }
+    public void empezaranimacion() {
+        new CountDownTimer(milisegundos, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                progreso.setProgress(establecer_progreso(millisUntilFinished));
+            }
+
+            @Override
+            public void onFinish() {
+                Intent nuevoFrom = new Intent(SplashScreen.this,principal.class);
+                startActivity(nuevoFrom);
+                finish();
+            }
+        }.start();
+
+    }
+
+    public int establecer_progreso(long miliseconds) {
+        return (int) ((milisegundos - miliseconds) / 1000);
+    }
+    public int maximo_progreso(){
+        return segundos-delay;
+    }
+}
---
+++ app/src/main/java/pe/edu/upc/clinicaupc/principal.java
@@ -0,0 +1,22 @@
+package pe.edu.upc.clinicaupc;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ProgressBar;
+
+/**
+ * Created by vjimenez on 21/05/2016.
+ */
+public class principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_principal);
+
+    }
+
+}
